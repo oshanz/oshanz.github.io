fa1c95a8bd258f9a471b2771f77ef6880d5a6267
oshanz <oshanz@users.noreply.github.com>
2025-08-13 13:35:52 +0000

    deploy: d058b2172b0e6a2733480339b76a7f70b2bcf099

---
+++ js/preferred-lang.js
@@ -0,0 +1,1 @@
+(()=>{var o={fallback:"/",homes:{en:"/"}};(()=>{let a=navigator.language||navigator.userLanguage;if(a in o.homes){window.location.href=o.homes[a];return}let e=a.split("-");for(let n in o.homes)if(n.indexOf(e[0])===0){window.location.href=o.homes[n];return}window.location.href=o.fallback})();})();
