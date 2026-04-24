1bb243a1d87eff96420ef560fb76f4a7301d460c
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-04-24 11:43:11 +1000

    add image convertor

---
+++ gallery-data/convert.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+in_dir  = File.join(__dir__, 'in')
+out_dir = File.join(__dir__, 'out')
+
+FileUtils.mkdir_p(in_dir)
+FileUtils.mkdir_p(out_dir)
+
+Dir.glob("#{in_dir}/**/*.{jpg,jpeg,png,JPG,JPEG,PNG}").each do |src|
+  rel = src.delete_prefix("#{in_dir}/")
+  dst = File.join(out_dir, File.dirname(rel), "#{File.basename(rel, '.*')}.webp")
+  FileUtils.mkdir_p(File.dirname(dst))
+  system('ffmpeg', '-y', '-i', src, '-vf', 'scale=400:-1', dst)
+end
