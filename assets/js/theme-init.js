dbe71eb2491e187166bb9a7c35cba94146aa209d
Oshan Wisumperuma <warunaoshan@gmail.com>
2026-04-27 15:29:24 +1000

    light theme as default

--- assets/js/theme-init.js
+++ assets/js/theme-init.js
@@ -1,4 +1,4 @@
 (function() {
-  const theme = localStorage.getItem('theme') || 'dark';
+  const theme = localStorage.getItem('theme') || 'light';
   document.documentElement.setAttribute('data-theme', theme);
 })();
